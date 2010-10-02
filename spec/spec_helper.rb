385073aa8a8f16a13bc4c751f4a79f2eab45da40
sebastien <s.weyn@mail.utexas.edu>
2010-10-01 20:08:46 -0500

    done with sign in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,6 +53,10 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   
   ActiveSupport::Dependencies.clear
+  
+  def test_sign_in(user)
+    controller.sign_in(user)
+  end
 end
 end
 
